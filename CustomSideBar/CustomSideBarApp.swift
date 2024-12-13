ec670a3b1e4f38e2b16946e9b73b84c70cd46508
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-12-13 19:59:43 +0500

    Initial Commit

---
+++ CustomSideBar/CustomSideBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSideBarApp.swift
+//  CustomSideBar
+//
+//  Created by A'zamjon Abdumuxtorov on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomSideBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
